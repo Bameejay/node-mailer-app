bf192d8440ad6ebc7b12fc9c40e4bba8e8a7a68b
Bameejay <johnsonkayode@gmail.com>
2023-07-25 14:42:37 +0100

    comment out cronjob

--- email.js
+++ email.js
@@ -1,31 +1,46 @@
 const nodemailer = require('nodemailer');
+// const cron = require('node-cron')
 
+var mailList = [
+    'jaysegs08@gmail.com',
+    'jaysegs30@gmail.com',
+  ];
+  
 let mailOptions = {
     from: 'bameejay@gmail.com',
-    to: 'jaysegs08@gmail.com',
+    to: mailList,
     subject: 'Email from Node-App: A Test Message!',
-    text: 'Some content to send',
+    text: 'Testing out nodemailer to work, Adding an attachment should be the next',
+    attachments: [
+        {
+          filename: 'Johnson Francis.docx', // Replace with your attachment file name
+          path: 'C:/Users/JOHNSON FRANCIS/Documents/Johnson Francis.docx', // Replace with the actual file path
+          // contentType: 'image/png' // Replace with the appropriate content type
+        }
+      ]
   };
 
-//   const transporter = nodemailer.createTransport({
-//     host: 'smtp.ethereal.email',
-//     port: 587,
-//     auth: {
-//         user: 'delbert.johnson68@ethereal.email',
-//         pass: 'jdFpbRGHYGNEXxu8vK'
-//     }
-// });
-
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, 
+    requireTLS: true,
+    logger: true,
+    debug: true,
     auth: {
-      user: 'bameejay@gmail.com',
-      pass: 'hzektpwlvmnbbdit'
-    }
-  });
+        user: 'bameejay@gmail.com',
+        pass: 'hzektpwlvmnbbdit'
+    },
+    tls: {
+        // do not fail on invalid certs
+        rejectUnauthorized: false
+    },
+    
+    });
 
-
-transporter.sendMail(mailOptions, (error, info) => {
-    if (error) console.log(error);
-    else console.log('Email sent: ' + info.response);
-  });
+// cron.schedule('0 */05 * * * *', () => {
+// transporter.sendMail(mailOptions, (error, info) => {
+//     if (error) console.log(error);
+//     else console.log('Email sent: ' + info.response);
+//   });
+// });
